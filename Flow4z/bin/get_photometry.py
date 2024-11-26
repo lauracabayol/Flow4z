ff86e4735b07857f72ba10ce9ed6e6ad30b93bef
Laura Cabayol Garcia <lauracabayol@Lauras-MacBook-Pro.local>
2024-11-26 17:23:11 +0100

    update code to get featarues

--- Flow4z/bin/get_photometry.py
+++ Flow4z/bin/get_photometry.py
@@ -16,8 +16,8 @@ logger.add(sys.stderr, format="{time} {level} {message}", level="INFO")
 os.environ["MLFLOW_TRACKING_URI"] = "http://127.0.0.1:5000"
 client = MlflowClient()
 
-from Flow4z.SBP import SBP
-from Flow4z.MBP import MBPz
+from Flow4z.SBP.SBP  import SBP
+from Flow4z.MBP.MBPz import MBPz
 
 
 def main():
@@ -71,54 +71,48 @@ def main():
     else:
         photometry_type = "MBP"
 
-    # Start an MLflow run
-    with mlflow.start_run():
-        # Log input arguments
-        mlflow.log_param("sbp_version", sbp_version)
-        mlflow.log_param("mbp_version", mbp_version)
-        mlflow.log_param("data_dir", data_dir)
+    if photometry_type == "SBP":
+        logger.info(
+            f"Running process_catalog with SBP model version: {sbp_version} and data directory: {data_dir}"
+        )
+        print("hello")
+        model = SBP(restore = True, 
+                    sbp_version = sbp_version)
+        flux_predictions, flux_predictions_err, true_fluxes, features = model.process_catalog(data_dir, 
+                                                                                                return_features)
 
-        if photometry_type == "SBP":
-            logger.info(
-                f"Running process_catalog with SBP model version: {sbp_version} and data directory: {data_dir}"
-            )
-            model = SBP(restore = True, 
-                        sbp_version = sbp_version)
-            flux_predictions, flux_predictions_err, true_fluxes, features = model.process_catalog(data_dir, 
-                                                                                                  return_features)
+        output_file = f"/nfs/pic.es/user/l/lcabayol/AI/Flow4z/catalogs/SBP_{dir_name}_{sbp_version}.csv"
+    elif photometry_type == "MBP":
+        logger.info(
+            f"Running process_catalog with MBP model version: {mbp_version} and data directory: {data_dir}"
+        )
 
-            output_file = f"/nfs/pic.es/user/l/lcabayol/AI/Flow4z/catalogs/SBP_{dir_name}_{sbp_version}.csv"
-        elif photometry_type == "MBP":
-            logger.info(
-                f"Running process_catalog with MBP model version: {mbp_version} and data directory: {data_dir}"
-            )
-
-            model_version_details = client.get_model_version('MBP', mbp_version)
-            run_id = model_version_details.run_id
-            run = client.get_run(run_id)
-            params = run.data.params
-            
-            model = MBPz(mbp_version=mbp_version,
-                        predict_photoz=eval(params['predict_photoz']),
-                        nexp=params['nexp'],
-                         
-                        )
-            output_file = f"/nfs/pic.es/user/l/lcabayol/AI/Flow4z/catalogs/MBP_{dir_name}_{mbp_version}.csv"
-
-            # Run model to process catalog and generate predictions
-            if eval(params['predict_photoz'])==True:
-                flux_predictions, flux_predictions_err, true_fluxes, photoz, photoz_err, redsfhit = model.process_catalog(data_dir)   
-                # Log results and save to file
-                catz = pd.DataFrame(
-                    np.c_[photoz.flatten(), photoz_err.flatten(), redsfhit.flatten()],
-                    columns=["photoz_pred", "photoz_err_pred", "redshift"],
-                )
-                output_file_z = f"/nfs/pic.es/user/l/lcabayol/AI/Flow4z/catalogs/z_{dir_name}_{mbp_version}.csv"
-                catz.to_csv(output_file_z, header=True, sep=",")
-            else:
-                flux_predictions, flux_predictions_err, true_fluxes = model.process_catalog(data_dir)
-
+        model_version_details = client.get_model_version('MBP', mbp_version)
+        run_id = model_version_details.run_id
+        run = client.get_run(run_id)
+        params = run.data.params
         
+        model = MBPz(mbp_version=mbp_version,
+                    predict_photoz=eval(params['predict_photoz']),
+                    nexp=params['nexp'],
+                        
+                    )
+        output_file = f"/nfs/pic.es/user/l/lcabayol/AI/Flow4z/catalogs/MBP_{dir_name}_{mbp_version}.csv"
+
+        # Run model to process catalog and generate predictions
+        if eval(params['predict_photoz'])==True:
+            flux_predictions, flux_predictions_err, true_fluxes, photoz, photoz_err, redsfhit = model.process_catalog(data_dir)   
+            # Log results and save to file
+            catz = pd.DataFrame(
+                np.c_[photoz.flatten(), photoz_err.flatten(), redsfhit.flatten()],
+                columns=["photoz_pred", "photoz_err_pred", "redshift"],
+            )
+            output_file_z = f"/nfs/pic.es/user/l/lcabayol/AI/Flow4z/catalogs/z_{dir_name}_{mbp_version}.csv"
+            catz.to_csv(output_file_z, header=True, sep=",")
+        else:
+            flux_predictions, flux_predictions_err, true_fluxes = model.process_catalog(data_dir)
+
+    
         # Log results and save to file
         cat = pd.DataFrame(
             np.c_[flux_predictions.flatten(), flux_predictions_err.flatten(), true_fluxes.flatten()],
