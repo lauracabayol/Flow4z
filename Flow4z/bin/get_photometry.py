02d16328eba4ebfc84c841de36dbf53a187f9d73
Laura Cabayol Garcia <lauracabayol@Lauras-MacBook-Pro.local>
2024-11-28 13:33:42 +0100

    bug bands

--- Flow4z/bin/get_photometry.py
+++ Flow4z/bin/get_photometry.py
@@ -85,7 +85,7 @@ def main():
         print("hello")
         model = SBP(restore = True, 
                     sbp_version = sbp_version,
-                    bands = [f"pau_nb{i}" for i in range(455,885,10)])
+                    bands = [f"pau_nb{i}" for i in range(455,855,10)])
         flux_predictions, flux_predictions_err, true_fluxes, features = model.process_catalog(data_dir, 
                                                                                                 metadata_dir,
                                                                                                 return_features)
