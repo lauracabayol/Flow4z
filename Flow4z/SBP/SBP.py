35c888816f7d65b07325ef85b5fdf29617f71260
Laura Cabayol Garcia <lauracabayol@Lauras-MacBook-Pro.local>
2024-11-28 14:07:45 +0100

    error when computing max norm

--- Flow4z/SBP/SBP.py
+++ Flow4z/SBP/SBP.py
@@ -73,7 +73,7 @@ class SBP:
                        nbands: int) -> Tuple[torch.Tensor, torch.Tensor, Optional[torch.Tensor]]:
         """Process a single batch of data."""
         z, lab = meta[:, 0, :, :], meta[:, 1, :, :]
-        lab = lab[:, :, 0].reshape(len(lab) * nbands)
+        lab = lab[:, :, 0].reshape(len(lab) , nbands)
         lab = lab / max_norm
         
         # Reshape stamp and lab
@@ -82,7 +82,7 @@ class SBP:
             .unsqueeze(1)
             .float()
         )
-        lab = lab.unsqueeze(1)
+        lab = lab.reshape(len(lab) * nbands).unsqueeze(1)
         
         # Handle zero-point calibration
         zp = None
