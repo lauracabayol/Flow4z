988ecdbb14b99c08a4bbccf9d2eb865228ded155
Laura Cabayol Garcia <lauracabayol@Lauras-MacBook-Pro.local>
2024-12-05 08:52:43 +0100

    ui script mlflow

--- Flow4z/utils/mlflow_ui.py
+++ Flow4z/utils/mlflow_ui.py
@@ -1,5 +1,23 @@
 import subprocess
 
 def start_mlflow_ui():
-    """Start the MLflow UI."""
-    subprocess.Popen(["mlflow", "ui"])
+    """Start the MLflow UI if not already running."""
+    # Check if mlflow UI is already running by trying to connect to default port
+    try:
+        import requests
+        requests.get("http://127.0.0.1:5000")
+        return None  # UI already running
+    except requests.exceptions.ConnectionError:
+        # Start mlflow UI since it's not running
+        process = subprocess.Popen(["mlflow", "ui"], cwd="../..")
+        return process
+
+def stop_mlflow_ui(process):
+    """Stop the MLflow UI.
+    
+    Args:
+        process: The process object returned by start_mlflow_ui()
+    """
+    if process:
+        process.terminate()
+        process.wait()
